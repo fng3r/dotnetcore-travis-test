e22db9c9356481db60e8033232cfaeb9b037bd70
Nagaev Dmitry <dinvk@mail.ru>
2018-03-24 01:48:19 +0500

    add tests, .travis.yml

---
+++ WebApp.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+
+namespace WebApp.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            "that string".Should().NotBeEmpty().And.HaveLength(11);
+        }
+    }
+}
